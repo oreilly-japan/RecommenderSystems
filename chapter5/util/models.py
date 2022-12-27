f5632a8ad23438bf64973103a7d4fd1cf88807e3
Moses Kim <moseskim@AL02273222.local>
2022-12-27 16:13:55 +0900

    updated *.py

--- chapter5/util/models.py
+++ chapter5/util/models.py
@@ -4,34 +4,34 @@ from typing import Dict, List
 
 
 @dataclasses.dataclass(frozen=True)
-# 推薦システムの学習と評価に使うデータセット
+# 추천 시스템의 학습과 평가에 사용하는 데이터셋
 class Dataset:
-    # 学習用の評価値データセット
+    # 학습용 평갓값 데이터셋
     train: pd.DataFrame
-    # テスト用の評価値データセット
+    # 테스트용 평갓값 데이터셋
     test: pd.DataFrame
-    # ランキング指標のテストデータセット。キーはユーザーID、バリューはユーザーが高評価したアイテムIDのリスト。
+    # 순위 지표의 테스트 데이터셋. 키는 사용자 ID, 값은 사용자가 높이 평가한 아이템의 ID 리스트
     test_user2items: Dict[int, List[int]]
-    # アイテムのコンテンツ情報
+    # 아이템 콘텐츠 정보
     item_content: pd.DataFrame
 
 
 @dataclasses.dataclass(frozen=True)
-# 推薦システムの予測結果
+# 추천 시스템 예측 결과
 class RecommendResult:
-    # テストデータセットの予測評価値。RMSEの評価
+    # 테스트 데이터셋의 예측 평갓값. RMSE 평가
     rating: pd.DataFrame
-    # キーはユーザーID、バリューはおすすめアイテムIDのリスト。ランキング指標の評価。
+    # 키는 사용자 ID, 값은 추천 아이템 ID 리스트. 순위 지표 평가.
     user2items: Dict[int, List[int]]
 
 
 @dataclasses.dataclass(frozen=True)
-# 推薦システムの評価
+# 추천 시스템 평가
 class Metrics:
     rmse: float
     precision_at_k: float
     recall_at_k: float
 
-    # 評価結果を出力する時に少数は第３桁までにする
+    # 평가 결과는 소수 셋째 자리까지만 출력한다
     def __repr__(self):
         return f"rmse={self.rmse:.3f}, Precision@K={self.precision_at_k:.3f}, Recall@K={self.recall_at_k:.3f}"
